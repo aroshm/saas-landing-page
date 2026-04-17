aff0600ff35f0fe3bc957edb577cc123169a65fc
Arosh Athukorala <aroshmathukorala@gmail.com>
2026-04-17 12:31:56 +0530

    added responsive changes for operations section

--- src/components/Operation.tsx
+++ src/components/Operation.tsx
@@ -9,27 +9,30 @@ const Operation = () => {
         title: "Project Discovery Call",
         description:
           "Party we years to order allow asked of. We so opinion friends me message as delight.",
-        className: "top-26 left-32",
+        className:
+          "relative md:absolute md:top-15 md:left-10 lg:top-10 lg:left-20 xl:top-19 xl:left-20 2xl:top-29 2xl:left-40",
       },
       {
         itemNumber: "2",
         title: "Project Discovery Call",
         description:
           "His defective nor convinced residence own. Connection has put impossible own apartments boisterous.",
-        className: "-top-8 left-155",
+        className:
+          "relative md:absolute md:top-0 md:left-70 lg:-top-18 lg:left-110 xl:-top-10 xl:left-125 2xl:-top-9 2xl:left-160",
       },
       {
         itemNumber: "3",
         title: "Project Discovery Call",
         description:
           "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly.",
-        className: "-top-73 -right-40",
+        className:
+          "relative md:absolute md:-top-23 md:right-0 lg:-top-60 xl:-top-45 xl:right-0 2xl:-top-72 2xl:-right-38",
       },
     ];
   return (
-    <section className="relative max-w-1920 mx-auto py-27 h-208">
+    <section className="relative max-w-1920 mx-auto py-16 md:py-27 md:h-208">
       <div className="max-w-6xl mx-auto px-6">
-        <div className="w-2/5">
+        <div className="md:w-3/5 lg:w-2/5">
           <p className="text-indigo-600 text-sm font-medium">
             STOCKIE OPERATION ACROSS THE WORLD
           </p>
@@ -48,11 +51,11 @@ const Operation = () => {
           </button>
         </div>
 
-        <div className="relative">
+        <div className="relative mt-8 md:mt-0">
           <img
             src={OperationCurve}
             alt="operation curve"
-            className="absolute -top-65 left-0 w-full"
+            className="absolute hidden md:block md:-top-35 left-0 w-full lg:-top-65"
           />
 
           {steps.map((step) => (
@@ -60,7 +63,7 @@ const Operation = () => {
               itemNumber={step.itemNumber}
               title={step.title}
               description={step.description}
-              className={`${step.className} w-69`}
+              className={`${step.className} w-full md:w-50 xl:w-69 mb-10 md:mb-0`}
             />
           ))}
         </div>
--- src/components/StepCard.tsx
+++ src/components/StepCard.tsx
@@ -12,11 +12,11 @@ const StepCard: React.FC<StepCardProps> = ({
   className,
 }) => {
   return (
-    <div className={`absolute ${className}`}>
-      <div className="absolute right-0 top-19 -z-1 leading-0 font-extrabold text-[204px] text-gray-200 tracking-normal text-right ">
+    <div className={`${className}`}>
+      <div className="absolute right-0 top-9 -z-1 leading-0 font-extrabold text-9xl xl:text-[204px] xl:top-19 text-gray-200 tracking-normal text-right ">
         {itemNumber}
       </div>
-      <div className="flex justify-center items-center w-16 h-16 mb-6 rounded-lg bg-white">
+      <div className="hidden md:flex justify-center items-center w-16 h-16 mb-6 rounded-lg bg-white">
         <div className="w-6 h-6 rounded-xl bg-gray-400"></div>
       </div>
       <h4 className="font-extrabold leading-7">{title}</h4>
